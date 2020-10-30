b9a93641db53c6f9a10eea5ee7e4a76e33ac54d0
Asharib Ahmed <Asharibahmed143@gmail.com>
2020-10-30 06:50:11 +0500

    finish

---
+++ app.py
@@ -0,0 +1,45 @@
+
+from flask import Flask, request, render_template,jsonify
+from flask_bootstrap import Bootstrap
+from keras.models import load_model
+import numpy as np
+
+app=Flask(__name__)
+
+
+def get_model():
+    global model
+    model = load_model('boston_house.h5')
+    print("Model Loaded")
+
+
+def text_preprocess(state):
+    value = np.float32([state['length'],state['width']])    
+    mean = value.mean()
+    value -= mean
+    std = value.std()
+    value /= std
+    value = np.append(value,[1,1,1,1,1,1,1,1,1,1,1] )
+    value=np.expand_dims(value, axis=0)
+    return value
+"""
+Routes
+"""
+@app.route('/',methods=['GET'])
+
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    state = request.get_json(force=True)
+    value = text_preprocess(state)
+    get_model()
+    result = model.predict(value)
+    result=result[0,0]    
+    return jsonify(str(result))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
